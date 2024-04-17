ec764d1f3ebb24e6b008be367566c14fe2f57d33
Bedizz <turkyilmaz.bediz@gmail.com>
2024-04-17 20:05:42 +0200

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar/navbar';
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/home'
+import Cart from './pages/Cart/cart'
+import PlaceOrder from './pages/PlaceOrder/placeOrder';
 
 function App() {
 
@@ -7,6 +11,11 @@ function App() {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/placeOrder' element={<PlaceOrder/>} />
+      </Routes>
       
     </div>
   )
---
+++ src/components/ExploreMenu/exploreMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './exploreMenu.css'
+
+const exploreMenu = () => {
+  return (
+    <div>exploreMenu</div>
+  )
+}
+
+export default exploreMenu
